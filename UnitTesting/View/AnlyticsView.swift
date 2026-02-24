98e36c0f0da58486026fd9cb515722a7315bcc21
Lokesh Yarashi <lyar0001@synchronoss.com>
2026-02-24 17:19:13 +0530

    Add analytic view

---
+++ UnitTesting/View/AnlyticsView.swift
@@ -0,0 +1,18 @@
+//
+//  AnlyticsView.swift
+//  UnitTesting
+//
+//  Created by Lokesh Yarashi on 24/02/26.
+//
+
+import SwiftUI
+
+struct AnlyticsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnlyticsView()
+}
