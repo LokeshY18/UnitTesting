3b2e5205eb16f5aa6f9f53552cf6a6f945932105
Lokesh Yarashi <lyar0001@synchronoss.com>
2026-02-24 17:10:08 +0530

    Add new swiftui view file

---
+++ UnitTesting/View/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  UnitTesting
+//
+//  Created by Lokesh Yarashi on 24/02/26.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
